92b2305e46099adbc26e394d1ad6dcb56475b6d5
Oscar Martinez Sanchez <omsz90@live.com.mx>
2020-03-24 20:06:54 -0600

    :package: Adding babel configuration and its initial files on the project
    babel configuration

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import pkg from '../package.json'
+
+console.log(pkg.description);
+console.log(`Version: ${pkg.version}`);
+console.log(`Author: ${pkg.author}`);
+console.log(`Licence: ${pkg.license}`);
+console.log(`Repository: ${pkg.homepage}`);
