40e74e820fee6acf4052ffb47b874c097759d730
Oscar Martinez Sanchez <omsz90@live.com.mx>
2020-05-13 21:09:53 -0500

    test: :construction: removing charAt method to implement bracket notation to access a char within a string
    implement bracket notation for string char access

--- src/CrackingCodeInterview/1.2.js
+++ src/CrackingCodeInterview/1.2.js
@@ -11,12 +11,12 @@ export default function checkPermutationOptimized(strOne, strTwo) {
   const strOneCounter = {};
 
   for (let i = 0; i < strOne.length; i += 1) {
-    const char = strOne.charAt(i);
+    const char = strOne[i];
     strOneCounter[char] = (strOneCounter[char] || 0) + 1;
   }
 
   for (let i = 0; i < strTwo.length; i += 1) {
-    strOneCounter[strTwo.charAt(i)] -= 1;
+    strOneCounter[strTwo[i]] -= 1;
     if (strOneCounter < 0) return false;
   }
 
