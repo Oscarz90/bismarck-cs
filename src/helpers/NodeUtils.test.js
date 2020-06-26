f99bdfecf51c2ca896caad10aff38eb8e4646b84
Oscarz90 <omsz90@live.com.mx>
2020-06-25 22:14:52 -0500

    feat: adding utils for linked list nodes

---
+++ src/helpers/NodeUtils.test.js
@@ -0,0 +1,30 @@
+import LinkedList from '@/DataStructures/LinkedList/LinkedList';
+import { expect } from 'chai';
+import { printNodeChain, reverseList } from './NodeUtils';
+
+describe('Node Utils', function () {
+  let testList;
+
+  beforeEach(function () {
+    testList = new LinkedList();
+    testList.add(5);
+    testList.add(3);
+    testList.add(1);
+    testList.add(8);
+    testList.add(6);
+  });
+
+  describe('printNodeChain', function () {
+    it('For list 5,3,1,8,6 should return 5->3->1->8->6', function () {
+      expect(printNodeChain(testList.head)).to.be.equal('5->3->1->8->6');
+    });
+  });
+
+  describe('reverseList', function () {
+    it('For list 5,3,1,8,6 should return 6->8->1->3->5', function () {
+      const newHead = reverseList(testList.head);
+
+      expect(printNodeChain(newHead)).to.be.equal('6->8->1->3->5');
+    });
+  });
+});
