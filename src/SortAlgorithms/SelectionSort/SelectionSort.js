ae68fc152e5f2745499cc8f293b498033d5ace2a
Oscar <omsz90@live.com.mx>
2020-06-26 00:08:19 -0500

    feat: selection sort algorithm (#56)
    * implementation of selection sort algorithm

---
+++ src/SortAlgorithms/SelectionSort/SelectionSort.js
@@ -0,0 +1,40 @@
+/**
+ * Find the index of the smallest value within a given list
+ * - Time complexity: O(n)
+ * @param {Array.<Number>} list - The given list to look within it the lowest value
+ * @returns {number} The index of the smallest value of the given list
+ */
+export function findLowest(list) {
+  let smallestIndex = 0;
+  let smallestValue = list[0];
+  for (let i = 0; i < list.length; i += 1) {
+    if (list[i] < smallestValue) {
+      smallestIndex = i;
+      smallestValue = list[i];
+    }
+  }
+
+  return smallestIndex;
+}
+
+/**
+ * Selection Sort Algorithm
+ * - Time complexity: O(n^2)
+ * @param {Array.<number>} list - A list of items to order
+ * @returns {Array.<number>} The list ordered from lowest to highest
+ */
+export default function SelectionSort(list) {
+  const newList = [];
+  let tempList = [...list];
+
+  while (tempList.length) {
+    const smallestIndex = findLowest(tempList);
+    newList.push(tempList[smallestIndex]);
+    tempList = [
+      ...tempList.slice(0, smallestIndex),
+      ...tempList.slice(smallestIndex + 1),
+    ];
+  }
+
+  return newList;
+}
